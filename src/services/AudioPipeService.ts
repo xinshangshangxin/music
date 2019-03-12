e6029014f05275d23a34d06ce1578cfa26ad9fbe
xinshangshangxin <xinshangshangxin@gmail.com>
2019-03-12 19:29:50 +0800

    fix(proxy): audio proxy response error

--- src/services/AudioPipeService.ts
+++ src/services/AudioPipeService.ts
@@ -49,7 +49,7 @@ async function audioPipe(
     res.setHeader('Content-Length', size);
   } else {
     res.statusCode = 206;
-    res.setHeader('Content-Length', len);
+    res.setHeader('Content-Length', end - start + 1);
 
     res.setHeader('Accept-Ranges', 'bytes');
     res.setHeader('Content-Range', `bytes ${start}-${end}/${size}`);
