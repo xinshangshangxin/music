e7848d899ed1b8d87da9aab284ddf1c9ab89f8d6
xinshangshangxin <xinshangshangxin@gmail.com>
2019-06-16 13:58:17 +0800

    feat: add graphql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { GraphQLModule } from './graphql/graphql.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    CoreModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule, GraphQLModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
