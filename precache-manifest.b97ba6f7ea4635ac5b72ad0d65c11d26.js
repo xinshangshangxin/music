0fe71c3b5ecde80d76a27f12ad11758779df2bb1
xinshangshangxin <xinshangshangxin@users.noreply.github.com>
2020-08-21 07:30:43 +0000

    Automated deployment: Fri Aug 21 07:30:43 UTC 2020 c7e7ffebb8ff42df3dac7d3d7b400c5d1833366e

---
+++ precache-manifest.b97ba6f7ea4635ac5b72ad0d65c11d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a3b5b4f3806b8c2708c1020db9d485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fa76a8769e9693fc97c",
+    "url": "/static/css/main.5bd1d67b.chunk.css"
+  },
+  {
+    "revision": "e7fcc504ee4485c7fb76",
+    "url": "/static/js/2.a722c075.chunk.js"
+  },
+  {
+    "revision": "d8d89644747ddd534f45f20a40dd34ae",
+    "url": "/static/js/2.a722c075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa76a8769e9693fc97c",
+    "url": "/static/js/main.e413aa19.chunk.js"
+  },
+  {
+    "revision": "9048dd95493fb2095d5d",
+    "url": "/static/js/runtime-main.a69dbb19.js"
+  }
+]);
