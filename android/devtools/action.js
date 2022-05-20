5790c10d17f2a0049c1001704b911bb04e8dbe76
xinshangshangxin <xinshangshangxin@gmail.com>
2022-05-20 15:33:42 +0800

    chore: rm androidjs build

--- android/devtools/action.js
+++
@@ -1,6 +0,0 @@
-const noop = () => {};
-
-process.stdout.clearLine = noop;
-process.stdout.cursorTo = noop;
-
-require("androidjs-builder/bin.js");
--- android/src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: 22519,
-  songUrl: 'http://music.xinshangshangxin.com/graphql'
-  // songUrl: 'http://localhost:3000/graphql'
-}
