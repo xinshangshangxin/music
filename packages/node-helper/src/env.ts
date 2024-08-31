3c9c06ebe46172aecf0fc189af03b775afe0bdba
feng <xinshangshangxin@gmail.com>
2024-08-31 21:15:13 +0800

    feat: add dotenv parse

--- packages/node-helper/src/env.ts
+++ packages/node-helper/src/env.ts
@@ -1,4 +1,8 @@
 import process from 'node:process';
+import { resolve } from 'node:path';
+import { existsSync } from 'node:fs';
+import { Errors } from 'helper';
+import { config } from 'dotenv';
 
 const ENV_NAME = (() => {
   const { NODE_ENV } = process.env;
@@ -25,4 +29,41 @@ const ENV_NAME = (() => {
   return name;
 })();
 
-export { ENV_NAME };
+interface Env {
+  HEADLESS_FILE_ROOT?: string;
+}
+
+function getEnvPath() {
+  const list = [
+    // 运行目录下
+    resolve(process.cwd(), `.env.${ENV_NAME}`),
+    // 当前目录下
+    resolve(__dirname, `.env.${ENV_NAME}`),
+    resolve(__dirname, '../', `.env.${ENV_NAME}`),
+    resolve(__dirname, '../../', `.env.${ENV_NAME}`),
+    resolve(__dirname, '../../../', `.env.${ENV_NAME}`),
+    resolve(__dirname, '../../../../', `.env.${ENV_NAME}`),
+  ];
+
+  for (const p of list) {
+    if (existsSync(p)) {
+      return p;
+    }
+  }
+
+  throw new Errors.EnvInvalid({ ENV_NAME, list }, 'env未找到');
+}
+
+const envPath = getEnvPath();
+
+// 获取环境变量
+const env = config({
+  path: envPath,
+})?.parsed as unknown as Env;
+
+if (!env) {
+  throw new Errors.EnvInvalid({ envPath }, 'env解析失败');
+}
+
+export type { Env };
+export { ENV_NAME, env };
